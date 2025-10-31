53c1384bba7a6d3819dc34568a354ed515545cda
Aniket Yadav <aniketyadavdv07@gmail.com>
2025-10-31 09:43:01 +0530

    fix(events): restore jQuery include for Bootstrap 4 modal compatibility
    - Re-add jQuery before Bootstrap JS to ensure modal and Bootstrap JS functionality work after refactor

--- front_zephyr/events.php
+++ front_zephyr/events.php
@@ -625,6 +625,7 @@ $csrf_token = generate_csrf_token();
         </div>
     </div>
     
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
     <script>
         // Utility: debounce
